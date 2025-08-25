3f9793884d03534b821ace473e54da43389bdc35
Jhnvida <joao.vida.andre@gmail.com>
2025-08-24 21:22:45 -0300

    :tada: Layout básico

---
+++ app/components/Habits.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-full py-12 px-16">
+        <div class="grid grid-cols-3 gap-2">
+            <p class="border border-gray-300 py-2 px-4">Aa</p>
+            <p class="border border-gray-300 py-2 px-4">Bb</p>
+            <p class="border border-gray-300 py-2 px-4">Cc</p>
+        </div>
+    </div>
+</template>
---
+++ app/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full flex-col items-center py-12 px-16">
+        <div class="flex w-full flex-row items-center justify-between">
+            <h1 class="transition-colors text-black text-xl font-medium">
+                Habit Tracker
+            </h1>
+            <button
+                class="bg-black text-white py-3 px-5 rounded-full text-sm font-medium cursor-pointer"
+                type="button"
+            >
+                New Habits
+            </button>
+        </div>
+    </div>
+</template>
