5e2b5b400e999bf8d3244d6b679232dd5aefc4ca
Jhnvida <joao.vida.andre@gmail.com>
2025-08-25 20:26:29 -0300

    :sparkles: Deletar hábitos

--- app/components/Habits.vue
+++ app/components/Habits.vue
@@ -6,8 +6,23 @@
                 class="flex flex-row items-center justify-between border border-gray-300 py-2 px-4"
             >
                 <p>{{ habit.name }}</p>
-                <input type="checkbox" v-model="habit.completed" />
+
+                <div class="flex gap-2">
+                    <Icon
+                        class="cursor-pointer"
+                        name="ic:baseline-delete"
+                        @click="removeHabit(habit.id)"
+                    />
+                    <input type="checkbox" v-model="habit.completed" />
+                </div>
             </div>
+
+            <button
+                class="bg-black text-white py-3 px-5 text-sm font-medium cursor-pointer"
+                type="button"
+            >
+                New Habits
+            </button>
         </div>
     </div>
 </template>
@@ -24,4 +39,8 @@ const habits = useState<Habit[]>("habits", () => [
     { id: 2, name: "Exercise", completed: false },
     { id: 3, name: "Read", completed: false },
 ]);
+
+function removeHabit(id: number) {
+    habits.value = habits.value.filter((habit) => habit.id !== id);
+}
 </script>
--- app/components/Header.vue
+++ app/components/Header.vue
@@ -4,12 +4,15 @@
             <h1 class="transition-colors text-black text-xl font-medium">
                 Habit Tracker
             </h1>
-            <button
-                class="bg-black text-white py-3 px-5 text-sm font-medium cursor-pointer"
-                type="button"
-            >
-                New Habits
-            </button>
+
+            <div class="flex flex-row items-center">
+                <a
+                    href="https://github.com/Jhnvida/habit-tracker"
+                    class="text-blue-500 hover:text-blue-700 mr-4"
+                    target="_blank"
+                    >GitHub</a
+                >
+            </div>
         </div>
     </div>
 </template>
