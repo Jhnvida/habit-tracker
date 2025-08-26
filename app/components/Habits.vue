5d29e25596bc9be32e9c1af1445cafc3a9a60979
Jhnvida <joao.vida.andre@gmail.com>
2025-08-25 21:53:15 -0300

    :sparkles: Edição de hábitos e ajustes

--- app/components/Habits.vue
+++ app/components/Habits.vue
@@ -3,11 +3,33 @@
         <div class="grid grid-cols-3 gap-2">
             <div
                 v-for="habit in habits"
-                class="flex flex-row items-center justify-between border border-gray-300 py-2 px-4"
+                class="flex flex-row items-center justify-between border py-2 px-4"
+                :class="habit.editing ? 'border-blue-400' : 'border-gray-300'"
             >
-                <p>{{ habit.name }}</p>
+                <p v-if="!habit.editing">{{ habit.name }}</p>
+
+                <input
+                    v-if="habit.editing"
+                    type="text"
+                    v-model="habit.name"
+                    class="w-full border-none outline-none focus:outline-none focus:ring-0 bg-transparent p-0"
+                />
 
                 <div class="flex gap-2">
+                    <Icon
+                        v-if="habit.editing"
+                        class="cursor-pointer"
+                        name="ic:baseline-save"
+                        @click="editHabit(habit.id, false)"
+                    />
+
+                    <Icon
+                        v-else
+                        class="cursor-pointer"
+                        name="ic:baseline-edit"
+                        @click="editHabit(habit.id, true)"
+                    />
+
                     <Icon
                         class="cursor-pointer"
                         name="ic:baseline-delete"
@@ -16,14 +38,16 @@
 
                     <Icon
                         v-if="habit.completed"
+                        class="cursor-pointer"
                         name="ic:baseline-check-box"
-                        @click="updateHabit(habit.id, !habit.completed)"
+                        @click="updateHabit(habit.id, false)"
                     />
 
                     <Icon
                         v-else
+                        class="cursor-pointer"
                         name="ic:baseline-check-box-outline-blank"
-                        @click="updateHabit(habit.id, !habit.completed)"
+                        @click="updateHabit(habit.id, true)"
                     />
                 </div>
             </div>
@@ -36,8 +60,6 @@
                 New Habits
             </button>
         </div>
-
-        <pre>{{ habits }}</pre>
     </div>
 </template>
 
@@ -48,9 +70,14 @@ interface Habit {
     id: number;
     name: string;
     completed: boolean;
+    editing: boolean;
 }
 
 const habits = useState<Habit[]>("habits", () => []);
+
+function updateStorage() {
+    localStorage.setItem("habits", JSON.stringify(habits.value));
+}
 
 function getHabits() {
     const storedHabits = localStorage.getItem("habits");
@@ -61,19 +88,32 @@ function getHabits() {
 }
 
 function addHabit(name: string) {
-    habits.value.push({ id: Date.now(), name, completed: false });
-    localStorage.setItem("habits", JSON.stringify(habits.value));
+    habits.value.push({
+        id: Date.now(),
+        name,
+        completed: false,
+        editing: false,
+    });
+
+    updateStorage();
 }
 
 function removeHabit(id: number) {
     habits.value = habits.value.filter((habit) => habit.id !== id);
-    localStorage.setItem("habits", JSON.stringify(habits.value));
+    updateStorage();
 }
 
 function updateHabit(id: number, completed: boolean) {
     const habit = habits.value.find((habit) => habit.id == id);
     if (habit) habit.completed = completed;
 
-    localStorage.setItem("habits", JSON.stringify(habits.value));
+    updateStorage();
+}
+
+function editHabit(id: number, editing: boolean) {
+    const habit = habits.value.find((habit) => habit.id == id);
+    if (habit) habit.editing = editing;
+
+    updateStorage();
 }
 </script>
